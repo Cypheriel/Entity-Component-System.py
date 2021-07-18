95281571830672297953355eea3e4fc4ffd10d30
Mikel <Mikel@hanger-84.com>
2021-07-18 02:38:48 -0600

    Export `Component`.

--- src/ecs/__init__.py
+++ src/ecs/__init__.py
@@ -1,4 +1,5 @@
 """Entity-Component System, which manages entities, components, and processors."""
+from ._component import Component
 from ._world import World
 
-__all__ = ["World"]
+__all__ = ["World", "Component"]
