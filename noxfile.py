ada5526cef9fb6651ef9f4a858b832a54a6d80e0
Mikel <Mikel@hanger-84.com>
2021-07-18 00:10:09 -0600

    Split linting, formatting, and mypy enforcement into multiple sessions.

--- noxfile.py
+++ noxfile.py
@@ -8,9 +8,24 @@ MISC: list[Union[str, PathLike[str]]] = ["./noxfile.py"]
 
 
 @nox.session
-def format_and_lint(session: nox.Session) -> None:
-    session.install("flake8", "black", "mypy")
-
+def black_format(session: nox.Session) -> None:
+    session.install("black")
     session.run("black", SOURCE, *MISC)
+
+
+@nox.session
+def lint(session: nox.Session) -> None:
+    session.install("flake8")
     session.run("flake8", "--max-line-length", "88", SOURCE, *MISC)
+
+
+@nox.session
+def mypy(session: nox.Session) -> None:
+    session.install("mypy")
     session.run("mypy", "--strict", "--python-version", "3.9", SOURCE)
+
+
+@nox.session
+def tests(session: nox.Session) -> None:
+    session.install("pytest", ".")
+    session.run("pytest")
