04ebf01e15cfeaf9fec712aafeab8b3a1d15e12a
Mikel <Mikel@hanger-84.com>
2021-07-18 01:25:40 -0600

    Fix `noxfile.py` exclusion

--- noxfile.py
+++ noxfile.py
@@ -4,25 +4,26 @@ from typing import Union
 import nox
 
 SOURCE: Union[str, PathLike[str]] = "./src/"
+TESTS: Union[str, PathLike[str]] = "./tests/"
 MISC: list[Union[str, PathLike[str]]] = ["./noxfile.py"]
 
 
 @nox.session
 def black_format(session: nox.Session) -> None:
     session.install("black")
-    session.run("black", SOURCE, *MISC)
+    session.run("black", SOURCE, TESTS, *MISC)
 
 
 @nox.session
 def lint(session: nox.Session) -> None:
     session.install("flake8")
-    session.run("flake8", "--max-line-length", "88", SOURCE, *MISC)
+    session.run("flake8", "--max-line-length", "88", SOURCE, TESTS)
 
 
 @nox.session
 def mypy(session: nox.Session) -> None:
     session.install("mypy")
-    session.run("mypy", "--strict", "--python-version", "3.9", SOURCE)
+    session.run("mypy", "--strict", "--python-version", "3.9", SOURCE, TESTS)
 
 
 @nox.session
