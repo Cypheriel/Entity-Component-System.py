cd06ed46f77825ccb3d04bceecc7add2f492eea1
Mikel <Mikel@hanger-84.com>
2021-07-18 02:40:25 -0600

    Add `Entity`-`Component` identifier test.

--- tests/entity_creation_test.py
+++ tests/entity_creation_test.py
@@ -1,13 +1,33 @@
 """Tests related to entity creation."""
-from ecs import World
+from dataclasses import dataclass
 
-world: World = World()
+from ecs import Component, World
+
+
+@dataclass
+class Position(Component):
+    """Position component."""
+
+    x: int
+    y: int
 
 
 def test_identifiers() -> None:
     """Ensure entity identifiers follow a predictable outcome."""
-    entity_1 = world.create_entity()
-    entity_2 = world.create_entity()
+    world: World = World()
 
-    assert entity_1 == 0
-    assert entity_2 == 1
+    assert world.create_entity() == 0
+    assert world.create_entity() == 1
+
+
+def test_component_identifiers() -> None:
+    """Ensure entity component identifiers match entity identifiers."""
+    world: World = World()
+
+    world.create_entity(Position(x=0, y=0))  # Entity-0
+    world.create_entity(Position(x=99, y=99))  # Entity-1
+
+    for position in world.get_component(Position):
+        predicted_position = [(0, 0), (99, 99)][position.entity_id]
+
+        assert predicted_position == (position.x, position.y)
